397d8408eeab72e8793c8dcec2670ece18248601
Julius Nehring-Wirxel <julius.nehring-wirxel@rwth-aachen.de>
2020-11-18 16:18:44 +0100

    Updated pcap reader.

--- src/babel-serializer/network/pcap.cc
+++ src/babel-serializer/network/pcap.cc
@@ -1,72 +1,38 @@
 #include "pcap.hh"
 
-babel::pcap::data babel::pcap::read(cc::span<const std::byte> data, read_config const& cfg, babel::error_handler on_error)
+#include <clean-core/utility.hh>
+
+namespace
 {
-    babel::pcap::data r;
-
-    read(data, [&](auto const& packet, auto) { r.packets.push_back(packet); }, [&](auto const& header) { r.header = header; }, cfg, on_error);
-
-    // copy raw data
-    if (!cfg.view_only)
-        r.bytes = data;
-
-    return r;
+template <class T>
+T byteswap(T v)
+{
+    static_assert(std::is_pod_v<T>);
+    std::byte d[sizeof(v)];
+    cc::as_byte_span(v).copy_to(cc::as_byte_span(d));
+    for (size_t i = 0; i < (sizeof(v) / 2); ++i)
+        cc::swap(d[i], d[sizeof(v) - 1 - i]);
+    cc::as_byte_span(v).copy_from(cc::as_byte_span(d));
+    return v;
+}
 }
 
-void babel::pcap::read(cc::span<std::byte const> data,
-                       cc::function_ref<void(data::packet const&, cc::span<std::byte const>)> on_packet,
-                       cc::function_ref<void(struct data::header const&)> on_header,
-                       babel::pcap::read_config const& cfg,
-                       babel::error_handler on_error)
+babel::pcap::detail::packet_iterator::packet_iterator(cc::span<const std::byte> data, const babel::pcap::read_config& cfg, babel::error_handler on_error, bool swap_endianness)
+  : _data{data}, _cfg{cfg}, _on_error{on_error}, _swap_endianness{swap_endianness}
 {
-    // TODO: more error handling
-    // TODO: Endianness, nanosecond timestamps (using magic number)
-
-    struct data::header h;
-
-    if (data.size() < sizeof(h))
-        on_error(data, data, "not enough data to read header", severity::error);
-
-    cc::as_byte_span(data, 0, sizeof(h)).copy_to(cc::as_byte_span(h));
-
-    CC_ASSERT(h.magic_number == 0xa1b2c3d4 && "currently only supports little endian and microsecond timestamps");
-
-    on_header(h);
-
-    // create packets
-
-    struct packet_header
-    {
-        uint32_t ts_sec;   /* timestamp seconds */
-        uint32_t ts_usec;  /* timestamp microseconds */
-        uint32_t incl_len; /* number of octets of packet saved in file */
-        uint32_t orig_len; /* actual length of packet */
-    };
-
-    size_t pos = sizeof(h);
-    while (pos < data.size())
-    {
-        packet_header header;
-        cc::as_byte_span(data, pos, sizeof(header)).copy_to(cc::as_byte_span(header));
-        pos += sizeof(header);
-
-        data::packet p;
-        p.timestamp_sec = header.ts_sec;
-        p.timestamp_usec = header.ts_usec;
-        p.stored_size = header.incl_len;
-        p.original_size = header.orig_len;
-        p.stored_offset = pos;
-
-        on_packet(p, data.subspan(p.stored_offset, p.stored_size));
-
-        pos += header.incl_len;
-    }
-
-    if (pos != data.size())
-        on_error(data, {}, "last packet defined more data than actually provided", severity::warning);
+    if (!_data.empty())
+        _current_packet = read_packet();
 }
 
-babel::pcap::reader::packet babel::pcap::reader::packet_iterator::read_packet()
+babel::pcap::detail::packet_iterator& babel::pcap::detail::packet_iterator::operator++()
+{
+    _pos += sizeof(packet_header) + _current_packet.data.size();
+    if (_pos < _data.size())
+        _current_packet = read_packet();
+    return *this;
+}
+
+babel::pcap::packet babel::pcap::detail::packet_iterator::read_packet()
 {
     packet_header ph;
     if (_pos + sizeof(ph) > _data.size())
@@ -80,10 +46,45 @@ babel::pcap::reader::packet babel::pcap::reader::packet_iterator::read_packet()
         _on_error(_data, _data.last(0), "Unexpected end of file", severity::warning);
         return {};
     }
+    if (_swap_endianness)
+    {
+        ph.incl_len = byteswap(ph.incl_len);
+        ph.orig_len = byteswap(ph.orig_len);
+        ph.ts_sec = byteswap(ph.ts_sec);
+        ph.ts_usec = byteswap(ph.ts_usec);
+    }
     packet p;
     p.original_size = ph.orig_len;
     p.timestamp_usec = ph.ts_usec;
     p.timestamp_sec = ph.ts_sec;
     p.data = _data.subspan(_pos + sizeof(ph), ph.incl_len);
     return p;
+}
+
+babel::pcap::header babel::pcap::header_of(cc::span<const std::byte> data, const babel::pcap::read_config& cfg, babel::error_handler on_error)
+{
+    (void)cfg; // for now unused
+
+    header h;
+    if (data.size() < sizeof(h))
+    {
+        on_error(data, data.last(0), "Unexpected end of file", severity::error);
+        return {};
+    }
+    data.subspan(0, sizeof(h)).copy_to(cc::as_byte_span(h));
+    if (h.swapped_endianness())
+    {
+        h.version_major = byteswap(h.version_major);
+        h.thiszone = byteswap(h.thiszone);
+        h.sigfigs = byteswap(h.sigfigs);
+        h.snap_length = byteswap(h.snap_length);
+        h.network = byteswap(h.network);
+    }
+    return h;
+}
+
+babel::pcap::detail::packet_range babel::pcap::packets_of(cc::span<const std::byte> data, const babel::pcap::read_config& cfg, babel::error_handler on_error)
+{
+    auto const h = header_of(data);
+    return detail::packet_range(data.subspan(sizeof(header)), cfg, on_error, h.swapped_endianness());
 }
--- src/babel-serializer/network/pcap.hh
+++ src/babel-serializer/network/pcap.hh
@@ -2,195 +2,114 @@
 
 #include <cstdint>
 
-#include <clean-core/array.hh>
-#include <clean-core/function_ref.hh>
-#include <clean-core/sentinel.hh>
 #include <clean-core/span.hh>
-#include <clean-core/vector.hh>
 
 #include <babel-serializer/errors.hh>
-#include <babel-serializer/file.hh>
 
 namespace babel::pcap
 {
-/// pcap reader class
-/// Usage:
-/// auto reader = reader("my/file/path.pcap");
-/// auto header = reader.header();
-/// for(auto p : reader.packets())
-///     my_process_package_data(p.data());
-/// NOTE: You can provide your own raw data by providing a cc::span<std::byte const> instead of a filepath
-/// NOTE: Currently only supports little endian and microsecond timestamps
-class reader
+namespace detail
 {
-public:
-    /// header of the entire pcap file
-    struct header
-    {
-        uint32_t magic_number;  ///< magic number
-        uint16_t version_major; ///< major version number
-        uint16_t version_minor; ///< minor version number
-        int32_t thiszone;       ///< GMT to local correction
-        uint32_t sigfigs;       ///< accuracy of timestamps
-        uint32_t snap_length;   ///< max length of captured packets, in octets
-        uint32_t network;       ///< data link type
-    };
-    /// single packet data
-    struct packet
-    {
-        uint32_t timestamp_sec = 0;     ///< timestamp seconds
-        uint32_t timestamp_usec = 0;    ///< timestamp microseconds
-        uint32_t original_size = 0;     ///< original length of packet (NOT the stored one)
-        cc::span<std::byte const> data; ///< raw data of the packet
-    };
+struct packet_range; // fwd
+}
 
-public: // ctor
-    /// allocating constructor that will read the whole file into memory
-    reader(cc::string_view filepath, error_handler on_error = default_error_handler)
-    {
-        _on_error = on_error;
-        _file_data = file::read_all_bytes(filepath, on_error); // should this be lazy instead?
-        _data = _file_data.data();
-        _size = _file_data.size();
-    }
-    /// non-allocating constructor that references into some other data
-    reader(cc::span<std::byte const> data, error_handler on_error = default_error_handler)
-    {
-        _on_error = on_error;
-        _data = data.data();
-        _size = data.size();
-    }
+struct header
+{
+    uint32_t magic_number;  ///< magic number
+    uint16_t version_major; ///< major version number
+    uint16_t version_minor; ///< minor version number
+    int32_t thiszone;       ///< GMT to local correction
+    uint32_t sigfigs;       ///< accuracy of timestamps
+    uint32_t snap_length;   ///< max length of captured packets, in octets
+    uint32_t network;       ///< data link type
 
-private:
-    struct packet_iterator
-    {
-        struct packet_header
-        {
-            uint32_t ts_sec;   /* timestamp seconds */
-            uint32_t ts_usec;  /* timestamp microseconds */
-            uint32_t incl_len; /* number of octets of packet saved in file */
-            uint32_t orig_len; /* actual length of packet */
-        };
+    /// returns true, if the endianness of all data must be swapped because the system that produced this
+    /// pcap file has opposite endianness than the system that is now reading the file
+    [[nodiscard]] constexpr bool swapped_endianness() const { return magic_number == 0xd4c3b2a1u || magic_number == 0x4d3cb2a1u; }
 
-        packet_iterator(cc::span<std::byte const> data, error_handler on_error) : _data{data}, _on_error{on_error}
-        {
-            if (!_data.empty())
-                _current_packet = read_packet();
-        }
+    /// Returns true, iff the packet timestamps are nanoseconds instead of microseconds
+    [[nodiscard]] constexpr bool nanosecond_timestamps() const { return magic_number == 0xa1b23c4du || magic_number == 0x4d3cb2a1u; }
+};
 
-        packet operator*() { return _current_packet; }
-
-        packet_iterator& operator++()
-        {
-            _pos += sizeof(packet_header) + _current_packet.data.size();
-            if (_pos < _data.size())
-                _current_packet = read_packet();
-            return *this;
-        }
-
-        packet_iterator operator++(int)
-        {
-            auto i = *this;
-            operator++();
-            return i;
-        }
-
-        bool operator!=(cc::sentinel) const { return _pos < _data.size(); }
-
-    private:
-        packet read_packet();
-        cc::span<std::byte const> _data;
-        packet _current_packet;
-        size_t _pos = 0;
-        error_handler _on_error;
-    };
-
-    struct packet_range
-    {
-        packet_range(cc::span<std::byte const> data, error_handler on_error) : data{data}, on_error{on_error} {}
-
-        cc::span<std::byte const> data; ///< data without the header
-        error_handler on_error;
-        packet_iterator begin() const { return {data, on_error}; }
-        cc::sentinel end() const { return {}; }
-    };
-
-public: // data
-    header header()
-    {
-        struct header h;
-        if (_size < sizeof(h))
-        {
-            _on_error(cc::span(_data, _size), cc::span(_data, _size).last(0), "Unexpected end of file", severity::error);
-            return {};
-        }
-        cc::as_byte_span(h).copy_from(cc::as_byte_span(_data, 0, sizeof(h)));
-        return h;
-    }
-
-    packet_range packets() { return {cc::span(_data, _size).subspan(sizeof(struct header)), _on_error}; }
-
-    std::byte const* data() const { return _data; }
-    size_t size() const { return _size; }
-
-private:
-    std::byte const* _data = nullptr;
-    size_t _size = 0;
-    cc::array<std::byte> _file_data; // only used when using the allocating constructor
-    error_handler _on_error = default_error_handler;
+struct packet
+{
+    uint32_t timestamp_sec = 0;     ///< timestamp seconds
+    uint32_t timestamp_usec = 0;    ///< timestamp microseconds (or nanoseconds, see header). Offset to seconds.
+    uint32_t original_size = 0;     ///< original length of packet (NOT the stored one)
+    cc::span<std::byte const> data; ///< raw data of the packet
 };
 
 struct read_config
 {
-    /// if true, does not copy data into bytes
-    /// packet::stored_offset and packet::stored_size can then be used into the data passed into read
-    bool view_only = false;
 };
 
-// see https://wiki.wireshark.org/Development/LibpcapFileFormat
-struct data
+/// usage:
+/// auto h = header_of(my_data);
+header header_of(cc::span<std::byte const> data, read_config const& cfg = {}, error_handler on_error = default_error_handler);
+
+/// usage:
+/// auto data = ...;
+/// for(auto p : packets_of(data))
+///     my_process_data(p.data());
+///
+/// IMPORTANT: data MUST survive the packet_range and its iterators!
+/// IMPORTANT: on_error MUST survive the packet_range and its iterators!
+///
+/// Usage with a custom error handler:
+/// auto data = ...;
+/// auto my_error_handler = [&](auto data, auto pos, auto message, auto severity){...};
+/// auto cfg = babel::pcap::read_config();
+/// for(auto p : packets_of(data, cfg, my_error_handler))
+///     my_process_data(p.data());
+detail::packet_range packets_of(cc::span<std::byte const> data, read_config const& cfg = {}, error_handler on_error = default_error_handler);
+
+namespace detail
 {
-    // NOTE: this is the global header of a pcap file
-    struct header
+struct packet_iterator
+{
+    struct packet_header
     {
-        uint32_t magic_number;  ///< magic number
-        uint16_t version_major; ///< major version number
-        uint16_t version_minor; ///< minor version number
-        int32_t thiszone;       ///< GMT to local correction
-        uint32_t sigfigs;       ///< accuracy of timestamps
-        uint32_t snap_length;   ///< max length of captured packets, in octets
-        uint32_t network;       ///< data link type
-    } header;
-
-    struct packet
-    {
-        uint32_t timestamp_sec;  ///< timestamp seconds
-        uint32_t timestamp_usec; ///< timestamp microseconds
-        uint32_t original_size;  ///< original length of packet (NOT the stored one)
-
-        uint32_t stored_size; ///< actual stored length of the packet (might have been truncated)
-        size_t stored_offset; ///< offset into data::bytes
+        uint32_t ts_sec;   /* timestamp seconds */
+        uint32_t ts_usec;  /* timestamp microseconds */
+        uint32_t incl_len; /* number of octets of packet saved in file */
+        uint32_t orig_len; /* actual length of packet */
     };
 
-    /// vector of all packets
-    cc::vector<packet> packets;
+    packet_iterator(cc::span<std::byte const> data, read_config const& cfg, error_handler on_error, bool swap_endianness);
+    packet operator*() { return _current_packet; }
+    packet_iterator& operator++();
+    packet_iterator operator++(int)
+    {
+        auto i = *this;
+        operator++();
+        return i;
+    }
+    bool operator!=(cc::sentinel) const { return _pos < _data.size(); }
 
-    /// returns a view on the packet data
-    /// NOTE: does not work if cfg.view_only was true
-    cc::span<std::byte const> packet_data(packet const& p) { return cc::as_byte_span(bytes).subspan(p.stored_offset, p.stored_size); }
-
-    /// raw bytes of the pcap file (indexed by packet data)
-    cc::array<std::byte> bytes;
+private:
+    packet read_packet();
+    cc::span<std::byte const> _data;
+    read_config const& _cfg; // currently unused
+    error_handler _on_error;
+    bool _swap_endianness;
+    packet _current_packet;
+    size_t _pos = 0;
 };
 
+struct packet_range
+{
+    cc::span<std::byte const> data; ///< data without the header
+    read_config const& cfg;
+    error_handler on_error;
+    bool swap_endianness;
 
-/// reads a pcap dump from memory
-data read(cc::span<std::byte const> data, read_config const& cfg = {}, error_handler on_error = default_error_handler);
+    packet_range(cc::span<std::byte const> data, read_config const& cfg, error_handler on_error, bool swap_endianness)
+      : data{data}, cfg{cfg}, on_error{on_error}, swap_endianness{swap_endianness}
+    {
+    }
 
-/// reads a pcap dump from memory calling on_header once for the header and then on_packet for each contained packet
-void read(cc::span<std::byte const> data,
-          cc::function_ref<void(data::packet const&, cc::span<std::byte const>)> on_packet,
-          cc::function_ref<void(struct data::header const&)> on_header = [](auto) {},
-          read_config const& cfg = {},
-          error_handler on_error = default_error_handler);
+    packet_iterator begin() const { return {data, cfg, on_error, swap_endianness}; }
+    cc::sentinel end() const { return {}; }
+};
+}
 }
